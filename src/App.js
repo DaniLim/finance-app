62d84ac12b084f189be6eadc7b661940c9f5f7e7
Dani Lim <danilimleal@gmail.com>
2024-10-27 18:45:22 +0100

    Refactor routing in App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Expenses from './pages/Expenses';
-import Budgets from './pages/Budgets';
-import Goals from './pages/Goals';
 
 function App() {
   return (
     <Router>
       <div>
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/expenses" component={Expenses} />
-          <Route path="/budgets" component={Budgets} />
-          <Route path="/goals" component={Goals} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/expenses" element={<Expenses />} />
+        </Routes>
       </div>
     </Router>
   );
 }
 
 export default App;
+
